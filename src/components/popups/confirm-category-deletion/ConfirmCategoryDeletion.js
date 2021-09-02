fbe1fc88c4253141b7c18f7636dc58f5a5cf43d3
Samvel <39568849+ssamvelk@users.noreply.github.com>
2021-09-03 00:07:33 +0400

    feat: add ConfirmCategoryDeletion popup

---
+++ src/components/popups/confirm-category-deletion/ConfirmCategoryDeletion.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  setIsPopupOpen,
+  deleteCategory,
+  selectActiveCategoryId,
+  selectCategoriesList,
+  selectIsPopupOpen,
+  setActiveCategoryId,
+} from '../../../store/categorySlice';
+import Button from '../../button/Button';
+import BasePopup from '../base-popup/BasePopup';
+
+import './ConfirmCategoryDeletion.scss';
+
+export default function ConfirmCategoryDeletion({ okHandler = null }) {
+  const dispatch = useDispatch();
+  const activeCategoryIdState = useSelector(selectActiveCategoryId);
+  const categoryList = useSelector(selectCategoriesList);
+
+  const cancel = () => {
+    console.log('Cancel');
+  };
+
+  return (
+    <BasePopup title='Are you want to delete the category?'>
+      {activeCategoryIdState}
+      <div className='confirm-category-deletion'>
+        <Button
+          text='Yes'
+          onClickHandle={() => {
+            okHandler();
+            // confirm();
+          }}
+        />
+        <Button
+          text='Cancel'
+          onClickHandle={() => dispatch(setIsPopupOpen(false))}
+        />
+      </div>
+    </BasePopup>
+  );
+}
