c8b20ddd8bcb8a660a10286ab07e573005a38ae9
Samvel <39568849+ssamvelk@users.noreply.github.com>
2021-09-03 22:00:45 +0400

    refactor: ProgressBar

--- src/components/progress-bar/ProgressBar.js
+++ src/components/progress-bar/ProgressBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectProgress } from '../../store/taskSlice';
 
 import './ProgressBar.scss';
 
-export default function ProgressBar(props) {
+export default function ProgressBar() {
+  const progress = useSelector(selectProgress);
   return (
     <div>
-      <progress value={props.value || 0} max={100} />
+      <progress value={progress || 0} max={100} />
     </div>
   );
 }
